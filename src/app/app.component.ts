25207cda13d90520e30faee68455d2a67feda1d5
swapnil29ahirrao <swapnil29ahirrao@gmail.com>
2019-09-01 19:52:03 +0530

    First check in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div>
+    Hello World
+  </div>
+  `
 })
 export class AppComponent {
   title = 'ng-fundamentals';
